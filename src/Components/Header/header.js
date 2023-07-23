15a89b1a94f7ada97c33295621feec2bf8965353
Aleksa013 <velma86@bk.ru>
2023-07-23 18:13:56 +0300

    feat:work with styles header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -2,6 +2,6 @@ import htmlToElements from "../../utils/htmlToElements";
 import Header from "./header.html";
 import "../../styles/header_main.scss";
 const headerPage = htmlToElements(Header);
-
+console.log(headerPage);
 
 export default headerPage;
