5489d658478da1d1f188bf58c3ca6a1e31d0fc7b
Aleksa013 <velma86@bk.ru>
2023-07-29 00:15:15 +0300

    feat:work with styles of card

--- src/Components/Card/card.js
+++ src/Components/Card/card.js
@@ -4,13 +4,15 @@ const CardComp= function(elem,name,linkImage){
     const card = document.createElement('div');
     card.classList.add('card');
     card.innerHTML=`
-    <img src=${linkImage} class="card-image" alt=${name} ></img>
+    <img src=${linkImage} class="card-image" alt=${name}></img>
     <h3 class="card-name">${name}</h3>
-    <div ></div>
+    <div class="wrapper-btn" ></div>
      `;
-    const cardBtn = card.querySelector('div');
-    console.log(cardBtn);
+    const petPic= card.querySelector('.card-image');
+    const cardBtn = card.querySelector('.wrapper-btn');
+   
     BtnComp(cardBtn ,'Learn more', 'card-button', 'card-button-text', '#');
+    
     elem.appendChild(card);
 
 };
