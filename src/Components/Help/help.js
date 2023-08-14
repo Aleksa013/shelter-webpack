6e44e43c956fc76255d11c42ab7fa073c86afe2b
Aleksa013 <velma86@bk.ru>
2023-08-14 18:46:30 +0300

    feat: section help done

---
+++ src/Components/Help/help.js
@@ -0,0 +1,7 @@
+import htmlToElements from "../../utils/htmlToElements";
+import Help from "./help.html";
+import  "../../styles/help.scss";
+
+const HelpOur= htmlToElements(Help);
+
+export default HelpOur;
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './styles/header_main.scss';
 import NotOnlyComponent from './Components/Not_only_people/not_only';
 import AboutComponent from './Components/About/about';
 import OurFriends from './Components/OurFriends/our_friends';
+import HelpOur from './Components/Help/help';
 
 
 const container = document.querySelector('.container');
@@ -11,4 +12,5 @@ const container = document.querySelector('.container');
 container.appendChild(NotOnlyComponent);
 container.appendChild(AboutComponent);
 container.appendChild(OurFriends);
+container.appendChild(HelpOur);
 console.log(NotOnlyComponent);
