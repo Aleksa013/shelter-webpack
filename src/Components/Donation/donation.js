2b0e8ca958af1a19fddefd29b4032914e935f757
Aleksa013 <velma86@bk.ru>
2023-08-15 18:56:41 +0300

    feat: section donation done

---
+++ src/Components/Donation/donation.js
@@ -0,0 +1,7 @@
+import Donation from "./donation.html"
+import "../../styles/donation.scss";
+import htmlToElements from "../../utils/htmlToElements";
+
+const DonationEl = htmlToElements(Donation);
+
+export default DonationEl;
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import NotOnlyComponent from './Components/Not_only_people/not_only';
 import AboutComponent from './Components/About/about';
 import OurFriends from './Components/OurFriends/our_friends';
 import HelpOur from './Components/Help/help';
-
+import DonationEl from './Components/Donation/donation';
 
 const container = document.querySelector('.container');
 
@@ -13,4 +13,5 @@ container.appendChild(NotOnlyComponent);
 container.appendChild(AboutComponent);
 container.appendChild(OurFriends);
 container.appendChild(HelpOur);
+container.appendChild(DonationEl);
 console.log(NotOnlyComponent);
