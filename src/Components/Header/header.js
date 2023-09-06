0f662cef6ef1465acfec98a559f9149d8da5547e
Aleksa013 <velma86@bk.ru>
2023-09-06 17:46:21 +0300

    feat:started work with function of burger

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -2,6 +2,14 @@ import htmlToElements from "../../utils/htmlToElements";
 import Header from "./header.html";
 
 const headerPage = htmlToElements(Header);
+const burger = headerPage.querySelector('.nav-burger'),
+      navList= headerPage.querySelector('.nav-list');
 
 
+burger.addEventListener('click',()=>{
+navList.classList.toggle('visible');
+navList.classList.toggle('hide');
+burger.classList.toggle('turn');
+});
+console.log(burger);
 export default headerPage;
