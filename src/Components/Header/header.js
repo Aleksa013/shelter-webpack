17b27202a145f2eede2490ad7e91ac69b08fded1
Aleksa013 <velma86@bk.ru>
2023-09-06 15:35:32 +0300

    feat: rtules for burger

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -2,6 +2,6 @@ import htmlToElements from "../../utils/htmlToElements";
 import Header from "./header.html";
 
 const headerPage = htmlToElements(Header);
-console.log(headerPage);
+
 
 export default headerPage;
