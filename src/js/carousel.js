52c46ff73c0b9f3b0aff2ef54dd9d04fab5cd548
Aleksa013 <velma86@bk.ru>
2023-09-27 11:50:17 +0300

    feat: add functios create sets

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -10,15 +10,32 @@ export const createSet= (set, data) =>{
       }
       };
 
-export const createAnotherSet = (anotherSet,set, data ) =>{
+export const createSetPast = (set, data ) =>{
+  const setPast=[];
   for(let i=0; i< 3; i++){
-    const randomIndex= Math.floor(Math.random() * data.length);
-    anotherSet.indexOf(randomIndex)!==-1?
-    i--:set.push(randomIndex);
-
-    set.indexOf(randomIndex)!==-1?
-    i--:set.push(randomIndex);
+    const randomAnotherIndex= Math.floor(Math.random() * data.length);
+    if(set.indexOf(randomAnotherIndex)!==-1){
+      i--;
+    }else{
+      setPast.indexOf(randomAnotherIndex)!==-1?
+      i--:setPast.push(randomAnotherIndex);
+    }  
   
   }
+  return setPast;
+};
+export const createSetNext = (set, data ) =>{
+  const setNext=[];
+  for(let i=0; i< 3; i++){
+    const randomAnotherIndex= Math.floor(Math.random() * data.length);
+    if(set.indexOf(randomAnotherIndex)!==-1){
+      i--;
+    }else{
+      setNext.indexOf(randomAnotherIndex)!==-1?
+      i--:setNext.push(randomAnotherIndex);
+    }  
+  
+  }
+  return setNext;
 };
 
